b045481240ed011f310e5accfe5e6f5fe07a84e0
diadistis <bsotirid@gmail.com>
2013-10-28 10:23:04 +0200

    Initial commit #2

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-dpd-proxy
-=========
+# Deployd Proxy Resource
 
-A simple proxy resource for deployd
+This custom resource type allows you to forward requests to another server.
+
+## Installation
+
+`$ npm install dpd-proxy`
+
+See [Installing Modules](http://docs.deployd.com/docs/using-modules/installing-modules.md) for details.
+
+## Configuration
+
+Before using the proxy resource, you must go to its Dashboard page and configure it.
+
+### Required settings:
+
+**host**  
+The hostname of your SMTP provider.
+
+### Optional settings:
+
+**username**  
+HTTP Basic Auth username.
+
+**password**  
+HTTP Basic Auth password.
---
+++ index.js
@@ -0,0 +1,65 @@
+var 
+    Resource = require('deployd/lib/resource'), 
+    util = require('util'),
+    http = require('http'),
+    url = require('url');
+
+function Proxy(name, options) {
+    Resource.apply(this, arguments);
+}
+util.inherits(Proxy, Resource);
+module.exports = Proxy;
+
+Proxy.prototype.clientGeneration = true;
+
+Proxy.prototype.handle = function (ctx, next) {
+    if(ctx.req && ctx.req.method !== 'GET') return next();
+
+    var urlObj = url.parse(this.config.url + ctx.url);    
+    urlObj.query = ctx.query;    
+    urlObj = url.parse(url.format(urlObj));
+    
+    var requestOptions = {
+        hostname : urlObj.host,
+        port : urlObj.port || 80,
+        path : urlObj.path,
+        method : ctx.req.method,
+        headers: {
+            'Authorization': 'Basic ' + new Buffer(this.config.username + ':' + this.config.password).toString('base64')
+        }    
+    };
+    
+    var proxy_request = http.request(requestOptions, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            ctx.res.write(chunk);
+        });
+        res.on('end', function (chunk) {
+            ctx.res.end();
+        });
+    });    
+    ctx.req.addListener('data', function(chunk) {
+        proxy_request.write(chunk, 'binary');
+    });
+    ctx.req.addListener('end', function() {
+        proxy_request.end();
+    });    
+};
+
+Proxy.basicDashboard = {
+  settings: [
+  {
+    name        : 'url',
+    type        : 'text',
+    description : 'the url'
+  }, {
+    name        : 'username',
+    type        : 'text',
+    description : 'HTTP username'
+  }, {
+    name        : 'password',
+    type        : 'text',
+    description : 'HTTP password'
+  }]
+};
+
